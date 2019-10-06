2e45664e7370114bebd7643ed37a396af4184caf
Matheus Kindrazki <matheuskindrazki@gmail.com>
2019-10-06 12:28:59 -0300

    rota para listagem de spots

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import uploadConfig from "./config/upload";
 
 import SessionController from "./controllers/SessionController";
 import SpotController from "./controllers/SpotController";
+import DashboardController from "./controllers/DashboardController";
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -11,5 +12,8 @@ const upload = multer(uploadConfig);
 routes.post("/sessions", SessionController.store);
 
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+routes.get("/spots", SpotController.index);
+
+routes.get("/dashboard", DashboardController.show);
 
 export default routes;
